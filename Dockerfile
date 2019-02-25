312dd285ad4760def81db1eb8ffd6e0b222e74f7
mickjm <mick.mozzland@virgin.net>
2019-02-25 09:02:44 +0000

    init

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ FROM alpine:3.8
 COPY mq-metrics-0.0.1-1.jar /usr/local/bin/
 #
 RUN ls -lrt /usr/local/bin/
+RUN ip addr show eth0
+RUN route
 #
 CMD ["java","-jar","/usr/local/bin/mq-metrics-0.0.1-1.jar"]
--- run-mq-metrics.cmd
+++ run-mq-metrics.cmd
@@ -2,6 +2,10 @@
 ::
 echo "INFO: Run mq-metrics01"
 ::
-docker run -d -e HOST=172.29.181.113 -e QMGR=TSTQPD01 -e CHANNEL=MQ.MONITOR.SVRCONN -e PORT=1414 -e USERID=MQMon01 -e PASSWORD=Passw0rd -p 8082:8082 --name=mq-metrics01 mq-metrics:01
+docker run --network=host -d -e HOST=172.17.228.113 -e QMGR=TSTQPD01 -e CHANNEL=MQ.MONITOR.SVRCONN -e PORT=1414 -e USERID=MQMon01 -e PASSWORD=Passw0rd -p 8082:8082 --name=mq-metrics01 mq-metrics:01
+
+docker run -d -e HOST=169.254.9.188 -e QMGR=TSTQPD01 -e CHANNEL=MQ.MONITOR.SVRCONN -e PORT=1414 -e USERID=MQMon01 -e PASSWORD=Passw0rd -p 8082:8082 --name=mq-metrics01 mq-metrics:01
 ::
-docker run -d -e HOST=172.29.181.113 -e QMGR=AOKQSC01 -e CHANNEL=MQ.MONITOR.SVRCONN -e PORT=1419 -e USERID=MQMon01 -e PASSWORD=Passw0rd -p 8092:8082 --name=mq-metrics02 mq-metrics:01
+::docker run -d -e HOST=172.16.2.113 -e QMGR=TSTQPD01 -e CHANNEL=MQ.MONITOR.SVRCONN -e PORT=1414 -e USERID=MQMon01 -e PASSWORD=Passw0rd -p 8082:8082 --name=mq-metrics01 mq-metrics:01
+::
+::docker run -d -e HOST=172.29.181.113 -e QMGR=AOKQSC01 -e CHANNEL=MQ.MONITOR.SVRCONN -e PORT=1419 -e USERID=MQMon01 -e PASSWORD=Passw0rd -p 8092:8082 --name=mq-metrics02 mq-metrics:01
