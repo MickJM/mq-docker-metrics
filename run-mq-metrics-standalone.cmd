8b39dedce1270c46084c23b915d7863621570b85
Mick Moriarty <mick.mozzland@virgin.net>
2019-08-31 10:43:41 +0100

    updates

--- run-mq-metrics-standalone.cmd
+++ run-mq-metrics-standalone.cmd
@@ -1,14 +1,16 @@
 ::
 echo "INFO: Run mq-metrics01"
 ::
+::set local
+::set spring.profiles.active=win-tstqpd01
+::
+::java -jar -Djavax.net.debug=none mq-metrics-0.0.1-SNAPSHOT.jar --spring.profiles.active=win-tstqpd01
+java -jar -Djavax.net.debug=none mq-metrics-0.0.1-SNAPSHOT.jar --spring.config.location=file:./config/ --spring.profiles.active=win-tstqpd01
+::
+::java -jar mq-metrics-0.0.1-SNAPSHOT.jar -Djavax.net.ssl.trustStore=C:\ProgramData\IBM\MQ\qmgrs\TSTQPD01\ssl\tstqpd01jks.jks -Djavax.net.ssl.keyStore=C:\ProgramData\IBM\MQ\qmgrs\TSTQPD01\ssl\tstqpd01jks.jks -Djavax.net.ssl.trustStorePassword=Passw0rd -Djavax.net.ssl.keyStorePassword=Passw0rd   
 ::docker-compose up -d
 ::docker run -d -v "C:\Users\mickm\OneDrive\Documents\Development\Docker\mq-metrics:/config/" -e spring.profiles.active=prod-qmap01 -p 8082:8082 --name=mq-metrics01 mq-metrics:01
-::
-:: dev-tstqpd01
 ::docker run -d -v "C:\Users\mickm\OneDrive\Documents\Development\Docker\mq-metrics:/config/" -e spring.profiles.active=dev-tstqpd01 -p 8082:8082 --name=mq-tstqpd01 mq-metrics:01
-::
-:: dev-qmap01
-docker run -d -v "C:\Users\mickm\OneDrive\Documents\Development\Docker\mq-metrics:/config/" -e spring.profiles.active=dev-qmap01 -p 8103:8082 --name=mq-qmap01 mq-metrics:01
 ::
 ::docker run -d -e spring.profiles.active=dev -e ibm.mq.connName=169.254.9.188(1442) -e ibm.mq.queueManager=QMAP01 -p 8082:8082 --name=mq-metrics01 mq-metrics:01
 ::
